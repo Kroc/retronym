07237a044597f84e1e9f5e1413c6f4a4b6959a3e
Kroc Camen <kroc@camendesign.com>
2018-09-07 10:17:18 +0100

    Crude list processing
    * REPL dumps result of parse
    * a list of atoms / numbers are parsed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 // retronym (C) copyright Kroc Camen 2017, 2018
 // BSD 2-clause licence; see LICENSE.TXT
 
-use std::io::{self, Write};
-
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use std::io::{self, Write};
 use pest::Parser;
+use pest::iterators::Pairs;
 
 // force the Rust compiler to acknowlege external changes to the grammer file
 // (this is recommended to do by Pest)
@@ -37,14 +37,37 @@ fn main() {
         print!("> ");
         io::stdout().flush().unwrap();
 
+        // get user input
         io::stdin().read_line(&mut line).unwrap();
-        // throw away the carriage-return
+        // (throw away the carriage-return)
         line.pop();
 
         // parse the line given
         match RymParser::parse(Rule::root, &line) {
-            Ok(pairs) => println!("{:?}", pairs),
+            Ok(pairs) => dump_pairs(pairs),
             Err(e) => println!("\n{}", e)
         };
     }
 }
+
+// pretty print the contents of Pairs.
+// I'm sure we can implement the Debug trait to do this more cleanly,
+// but I don't know how to do that just yet
+fn dump_pairs(pairs: Pairs<Rule>) {
+    // loop over our Pairs
+    for pair in pairs.flatten() {
+        // a pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            println!("= {:?}", inner_pair.as_rule());
+
+            let inner_span = inner_pair.clone().into_span();
+            println!("  \"{}\"", inner_span.as_str());
+            
+            /*match inner_pair.as_rule() {
+                Rule::alpha => println!("Letter:  {}", inner_span.as_str()),
+                Rule::digit => println!("Digit:   {}", inner_span.as_str()),
+                _ => unreachable!()
+            };*/
+        }
+    }
+} 
